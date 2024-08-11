ab761c9aa4348045594df585e2bcf8b365abf84f
Cade Gilbert <hex.cadeg@gmail.com>
2024-08-10 21:11:01 -0400

    Added updater function, and some personal flair

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Media;
 using Newtonsoft.Json;
 using System.Windows.Media.Imaging;
 using System.Diagnostics;
+using System.IO.Compression;
 using System.Numerics;
 using System.Windows.Threading;
 
@@ -300,90 +301,6 @@ namespace StartGGgraphicGenerator
                             events {
                                 id
                                 name
-                            }
-                        }
-                    }"
-                };
-
-                var jsonContent = JsonConvert.SerializeObject(query);
-                var content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
-
-                var response = await client.PostAsync(apiUrl, content);
-                response.EnsureSuccessStatusCode();
-
-                var result = await response.Content.ReadAsStringAsync();
-                var graphQLResponse = JsonConvert.DeserializeObject<GraphQLResponse>(result);
-
-                if (graphQLResponse?.Data?.Tournament?.Events != null)
-                {
-                    events = graphQLResponse.Data.Tournament.Events;
-                    Log($"Fetched {events.Count} events.");
-                }
-                else
-                {
-                    Log("No events found for this tournament.");
-                    MessageBox.Show("No events found for this tournament.");
-                }
-            }
-        }
-
-        private async Task FetchEventsFromLeague(string slug)
-        {
-            using (var client = new HttpClient())
-            {
-                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apiToken);
-
-                var query = new
-                {
-                    query = @"
-                    {
-                        league(slug: """ + slug + @""") {
-                            events {
-                                nodes {
-                                    id
-                                    name
-                                }
-                            }
-                        }
-                    }"
-                };
-
-                var jsonContent = JsonConvert.SerializeObject(query);
-                var content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
-
-                var response = await client.PostAsync(apiUrl, content);
-                response.EnsureSuccessStatusCode();
-
-                var result = await response.Content.ReadAsStringAsync();
-                var graphQLResponse = JsonConvert.DeserializeObject<GraphQLResponse>(result);
-
-                if (graphQLResponse?.Data?.League?.Events?.Nodes != null)
-                {
-                    events = graphQLResponse.Data.League.Events.Nodes;
-                    Log($"Fetched {events.Count} events.");
-                }
-                else
-                {
-                    Log("No events found for this league.");
-                    MessageBox.Show("No events found for this league.");
-                }
-            }
-        }
-
-        private async Task FetchStandingsFromTournament(string slug)
-        {
-            using (var client = new HttpClient())
-            {
-                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apiToken);
-
-                var query = new
-                {
-                    query = @"
-                    {
-                        tournament(slug: """ + slug + @""") {
-                            events {
-                                id
-                                name
                                 standings(query: {perPage: 100, page: 1}) {
                                     nodes {
                                         entrant {
@@ -438,7 +355,7 @@ namespace StartGGgraphicGenerator
             }
         }
 
-        private async Task FetchStandingsFromLeague(string slug)
+        private async Task FetchEventsFromLeague(string slug)
         {
             using (var client = new HttpClient())
             {
@@ -449,18 +366,81 @@ namespace StartGGgraphicGenerator
                     query = @"
                     {
                         league(slug: """ + slug + @""") {
-                            standings(query: {perPage: 100, page: 1}) {
+                            events {
                                 nodes {
-                                    player {
-                                        prefix
-                                        gamerTag
+                                    id
+                                    name
+                                    standings(query: {perPage: 100, page: 1}) {
+                                        nodes {
+                                            entrant {
+                                                id
+                                                name
+                                            }
+                                            placement
+                                            totalPoints
+                                        }
                                     }
-                                    placement
-                                    totalPoints
                                 }
                             }
                         }
                     }"
+                };
+
+                var jsonContent = JsonConvert.SerializeObject(query);
+                var content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+
+                var response = await client.PostAsync(apiUrl, content);
+                response.EnsureSuccessStatusCode();
+
+                var result = await response.Content.ReadAsStringAsync();
+                var graphQLResponse = JsonConvert.DeserializeObject<GraphQLResponse>(result);
+
+                events.Clear();
+
+                if (graphQLResponse?.Data?.League?.Events?.Nodes != null)
+                {
+                    foreach (var evt in graphQLResponse.Data.League.Events.Nodes)
+                    {
+                        events.Add(new Event
+                        {
+                            Id = evt.Id,
+                            Name = evt.Name,
+                            Standings = evt.Standings
+                        });
+                    }
+                    Log($"Fetched {events.Count} events.");
+                }
+                else
+                {
+                    Log("No events found for this league.");
+                    MessageBox.Show("No events found for this league.");
+                }
+            }
+        }
+
+        private async Task FetchStandingsFromLeague(string slug)
+        {
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apiToken);
+
+                var query = new
+                {
+                    query = @"
+            {
+                league(slug: """ + slug + @""") {
+                    standings(query: {perPage: 100, page: 1}) {
+                        nodes {
+                            player {
+                                prefix
+                                gamerTag
+                            }
+                            placement
+                            totalPoints
+                        }
+                    }
+                }
+            }"
                 };
 
                 var jsonContent = JsonConvert.SerializeObject(query);
@@ -492,6 +472,73 @@ namespace StartGGgraphicGenerator
                 {
                     Log("No standings found for this league.");
                     MessageBox.Show("No standings found for this league.");
+                }
+            }
+        }
+
+
+        private async Task FetchStandingsFromTournament(string slug)
+        {
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apiToken);
+
+                var query = new
+                {
+                    query = @"
+            {
+                tournament(slug: """ + slug + @""") {
+                    events {
+                        standings(query: {perPage: 100, page: 1}) {
+                            nodes {
+                                entrant {
+                                    id
+                                    name
+                                }
+                                placement
+                                totalPoints
+                            }
+                        }
+                    }
+                }
+            }"
+                };
+
+                var jsonContent = JsonConvert.SerializeObject(query);
+                var content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+
+                var response = await client.PostAsync(apiUrl, content);
+                var result = await response.Content.ReadAsStringAsync();
+                Log($"Tournament standings response: {result}");
+                response.EnsureSuccessStatusCode();
+
+                var graphQLResponse = JsonConvert.DeserializeObject<GraphQLResponse>(result);
+
+                players.Clear();
+
+                if (graphQLResponse?.Data?.Tournament?.Events != null)
+                {
+                    foreach (var evt in graphQLResponse.Data.Tournament.Events)
+                    {
+                        if (evt.Standings?.Nodes != null)
+                        {
+                            foreach (var node in evt.Standings.Nodes)
+                            {
+                                players.Add(new Player
+                                {
+                                    Name = node.Entrant.Name,
+                                    Placement = node.Placement,
+                                    Points = node.TotalPoints
+                                });
+                            }
+                        }
+                    }
+                    Log($"Fetched standings from {graphQLResponse.Data.Tournament.Events.Count} events.");
+                }
+                else
+                {
+                    Log("No standings found for this tournament.");
+                    MessageBox.Show("No standings found for this tournament.");
                 }
             }
         }
@@ -672,6 +719,55 @@ namespace StartGGgraphicGenerator
                 }
             }
         }
+
+        private async void UpdateButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string updateUrl = "https://github.com/CadeXR/StartGGgraphicGenerator/releases/download/untagged-42c06126b4552113e2f0/StartGGgraphicGenerator.zip";
+
+                string tempZipPath = Path.Combine(Path.GetTempPath(), "StartGGgraphicGeneratorUpdate.zip");
+                string extractPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "UpdateTemp");
+
+                using (var client = new HttpClient())
+                {
+                    Log("Downloading update...");
+                    var response = await client.GetAsync(updateUrl);
+                    response.EnsureSuccessStatusCode();
+
+                    using (var fs = new FileStream(tempZipPath, FileMode.Create))
+                    {
+                        await response.Content.CopyToAsync(fs);
+                    }
+                }
+
+                Log("Extracting update...");
+                if (Directory.Exists(extractPath))
+                {
+                    Directory.Delete(extractPath, true);
+                }
+                ZipFile.ExtractToDirectory(tempZipPath, extractPath);
+
+                Log("Replacing old files...");
+                foreach (var file in Directory.GetFiles(extractPath))
+                {
+                    var fileName = Path.GetFileName(file);
+                    if (fileName != "settings.txt")
+                    {
+                        var destinationPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, fileName);
+                        File.Copy(file, destinationPath, true);
+                    }
+                }
+
+                Log("Update completed. Please restart the application.");
+                MessageBox.Show("Update completed. Please restart the application.", "Update", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            catch (Exception ex)
+            {
+                Log($"Update failed: {ex.Message}");
+                MessageBox.Show($"Update failed: {ex.Message}", "Update Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
     }
 
     public class Player
@@ -691,13 +787,6 @@ namespace StartGGgraphicGenerator
         public Event Event { get; set; }
         public Tournament Tournament { get; set; }
         public League League { get; set; }
-    }
-
-    public class Event
-    {
-        public string Id { get; set; }
-        public string Name { get; set; }
-        public Standings Standings { get; set; }
     }
 
     public class Tournament
@@ -738,5 +827,12 @@ namespace StartGGgraphicGenerator
     {
         public string Prefix { get; set; }
         public string GamerTag { get; set; }
+    }
+
+    public class Event
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public Standings Standings { get; set; } // Added Standings property
     }
 }
